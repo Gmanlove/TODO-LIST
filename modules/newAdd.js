93d35da9d28afd7af018edb1937dd02e67bcf773
Gmanlove <emmagman3@gmail.com>
2023-02-23 22:59:23 +0100

    Fix linter error

--- modules/newAdd.js
+++ modules/newAdd.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-mutable-exports */
 const submit = document.querySelector('.uzo');
 const tasksDiv = document.querySelector('.todos');
 export const taskInput = document.querySelector('.add-todo input');
